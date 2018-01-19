5e21026c912fb47db5ea1633e8144f6b00eff3af
blackle <isabelle@blackle-mori.com>
2018-01-18 21:53:33 -0500

    Added CollectiveStatistics and TransitivityDeviationCalculator

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@ from typing import NewType, Tuple, Dict, List
 
 PairwiseSample = Tuple[int, int]
 CandyId = NewType('CandyId', int)
+CandyIdDict = NewType('CandyIdDict', Dict[CandyId, int])
 CandyTuple = NewType('CandyTuple', Tuple[CandyId, CandyId])
 CandyTupleList = NewType('CandyTupleList', List[CandyTuple])
 CandyTupleDict = NewType('CandyTupleDict', Dict[CandyTuple, PairwiseSample])
@@ -18,6 +19,9 @@ class CandyDatabase:
 		"Sour Patch Kids",
 		"Skittles"
 	]
+
+	def candyIds() -> List[CandyId]:
+		return [CandyDatabase.idForCandy(x) for x in CandyDatabase.__candies]
 
 	def idForCandy(candy : str) -> CandyId:
 		return CandyId(CandyDatabase.__candies.index(candy))
@@ -36,6 +40,28 @@ class CandyDatabase:
 		candy_a = CandyDatabase.candyForId(tuple[0])
 		candy_b = CandyDatabase.candyForId(tuple[1])
 		return "{0} or {1}?".format(candy_a, candy_b)
+
+
+class CollectiveStatistics:
+	def __init__(self):
+		self.__dict = CandyIdDict({})
+		self.__total = 0
+
+		for id in CandyDatabase.candyIds():
+			self.__dict[id] = 0
+
+	def processSample(self, winner : CandyId) -> None:
+		self.__total += 1
+		self.__dict[winner] += 1
+
+	def total(self) -> int:
+		return self.__total
+
+	def lookup(self, id : CandyId) -> int:
+		return self.__dict[id]
+
+	def lookupTuple(self, tuple : CandyTuple) -> PairwiseSample:
+		return (self.lookup(tuple[0]), self.lookup(tuple[1]))
 
 
 class PairwiseStatistics:
@@ -67,25 +93,53 @@ class PairwiseStatistics:
 		return sorted(list(self.__dict.keys()))
 
 
-def main():
-	stats = PairwiseStatistics()
+class TransitivityDeviationCalculator:
+	def __init__(self, col_stats : CollectiveStatistics, pair_stats : PairwiseStatistics):
+		self.__col_stats = col_stats
+		self.__pair_stats = pair_stats
 
-	with open('data.csv', 'r') as csvfile:
+	def lookup(self, key : CandyTuple) -> float:
+		pair_a, pair_b = self.__pair_stats.lookup(key)
+		col_a, col_b = self.__col_stats.lookupTuple(key)
+
+		pair_total = pair_a + pair_b
+		col_total = col_a + col_b
+
+		bias = (pair_a / pair_total) - (col_a / col_total)
+
+		return bias
+
+def csv_parser(filename : str) -> (CollectiveStatistics, PairwiseStatistics):
+	col_stats = CollectiveStatistics()
+	pair_stats = PairwiseStatistics()
+
+	with open(filename, 'r') as csvfile:
 		data = csv.DictReader(csvfile)
 
 		for row in data:
 			for field in data.fieldnames:
-				if field == "Choose your most preferred candy" or field == "Timestamp":
-					continue
+				if field == "Choose your most preferred candy":
+					row_winner = CandyDatabase.idForCandy(row[field])
+					col_stats.processSample(row_winner)
 
-				header_tuple = CandyDatabase.tupleForHeader(field)
-				row_winner = CandyDatabase.idForCandy(row[field])
-				stats.processSample(header_tuple, row_winner)
+				elif " or " in field:
+					header_tuple = CandyDatabase.tupleForHeader(field)
+					row_winner = CandyDatabase.idForCandy(row[field])
+					pair_stats.processSample(header_tuple, row_winner)
 
-	for key in stats.keys():
-		candy_a, candy_b = key
-		count_a, count_b = stats.lookup(key)
-		print(CandyDatabase.headerForTuple(key), stats.lookup(key))
+	return (col_stats, pair_stats)
+
+
+def main():
+	col_stats, pair_stats = csv_parser('data.csv')
+
+	trans_dev_calc = TransitivityDeviationCalculator(col_stats, pair_stats)
+
+	for key in pair_stats.keys():
+
+		print(CandyDatabase.headerForTuple(key))
+		print(trans_dev_calc.lookup(key))
+
 
 if __name__ == "__main__":
 	main()
